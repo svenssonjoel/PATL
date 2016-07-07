9beb1bf2eee1189397a766b19b80a832262a2986
Joel Svensson <svenssonjoel@yahoo.se>
2016-07-07 13:31:18 +0200

    update

--- PATL/GenOpenCL.hs
+++ PATL/GenOpenCL.hs
@@ -5,3 +5,9 @@ module PATL.GenOpenCL where
 
 import Language.C.Quote.OpenCL
 import PATL.AST
+
+
+-------------------------------------------------------
+-- Compilation
+--
+--  TODO: Fusion of patterns (Separate module ?) 
