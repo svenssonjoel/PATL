ef8ff0b492bec401d6173875e2b18489d715899c
Joel Svensson <svenssonjoel@yahoo.se>
2016-07-12 15:14:37 +0200

    graph reify

--- PATL/EDSL/Compile.hs
+++ PATL/EDSL/Compile.hs
@@ -6,13 +6,26 @@ import PATL.EDSL.Shape
 
 import qualified PATL.AST as A 
 import qualified PATL.EDSL as E
+import PATL.EDSL.Syntax 
 
 
 import Data.Reify
 import Data.Reify.Graph
 
 
+-- This is the end goal
 --compile :: E.Exp a -> A.Exp
 --compile = undefined 
 
 
+-- ------------------------------------------------------------
+--
+-- ------------------------------------------------------------
+
+instance MuRef Expr where
+  type DeRef Expr = Syntax
+  mapDeRef f (Expr s) = traverse f s
+
+
+genGraph :: MuRef s => s -> IO (Graph (DeRef s))
+genGraph = reifyGraph 
