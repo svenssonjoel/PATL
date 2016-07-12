5ad3c50b4daaeeed9043c8d034b467589d34ea8d
Joel Svensson <svenssonjoel@yahoo.se>
2016-07-12 11:11:42 +0200

    working on embedding

--- PATL/EDSL.hs
+++ PATL/EDSL.hs
@@ -10,7 +10,9 @@ import PATL.EDSLSyntax hiding (IRange, IIndex)
 import qualified PATL.EDSLSyntax as S 
 
 import Prelude hiding (map) 
-import qualified Prelude as P 
+import qualified Prelude as P
+
+import qualified Data.Set as Set 
 
 
 data Array sh a 
@@ -51,6 +53,21 @@ foldExpr f a (Expr e) = fse (\i j -> f i (Expr j))  a e
     fse f a e@(S.IIndex s) = f (f a (syntax s)) e 
     fse f a e@(S.IRange s1 s2) = f (foldl f a (P.map syntax [s1,s2])) e
     fse f a e@(Op _ es)  = f (foldl f a (P.map syntax es)) e
-    fse f a e@(Lam id s) = f (f a (syntax s)) e 
-    fse f a e = f a e 
+    fse f a e@(Lam id s) = f (f a (syntax s)) e
+    fse f a e@(App s1 s2) = f (foldl f a (P.map syntax [s1,s2])) e
+    fse f a e@(Iota s)  = f (f a (syntax s)) e
+    fse f a e@(Prj s1 s2) = f (foldl f a (P.map syntax [s1,s2])) e
+    fse f a e@(SizeOf s) = f (f a (syntax s)) e
+    fse f a e@(Generate s1 s2) = f (foldl f a (P.map syntax [s1,s2])) e
+    fse f a e@(Map s1 s2) = f (foldl f a (P.map syntax [s1,s2])) e
+    fse f a e@(ZipWith s1 s2 s3) = f (foldl f a (P.map syntax [s1,s2,s3])) e
+    fse f a e@(Reduce s1 s2 s3) = f (foldl f a (P.map syntax [s1,s2,s3])) e
+    fse f a e = f a e -- nonrecursive cases 
 
+-- Collect identifier 
+idCollector :: Syntax Expr -> Set.Set Identifier
+idCollector s = doIt Set.empty s
+  where
+    doIt set (Var id) =  Set.insert id set
+    doIt set (Lam id _) = Set.insert id set
+    doIt set _ = set 
