22b9cd7d0d3acbd47eedaaf6d26c4a9ae5544c91
Joel Svensson <svenssonjoel@yahoo.se>
2016-07-12 12:16:06 +0200

    working on the embedding

--- PATL/EDSL.hs
+++ PATL/EDSL.hs
@@ -5,11 +5,12 @@
 module PATL.EDSL where
 
 import PATL.Value
+import PATL.Operators 
 import PATL.EDSL.Shape
 import PATL.EDSL.Syntax hiding (IRange, IIndex) 
 import qualified PATL.EDSL.Syntax as S 
 
-import Prelude hiding (map) 
+import Prelude hiding (map, zipWith, div ) 
 import qualified Prelude as P
 
 
@@ -41,6 +42,7 @@ generate sh f =  Exp $ Expr $ Generate (Expr (Constant (VInt 1)))
 -- ------------------------------------------------------------
 class Expable a where
   toExp :: a -> Expr
+  
 
 instance Expable (Exp a) where
   toExp = unExp
@@ -56,3 +58,25 @@ instance Expable a => Expable (I a) where
   toExp = undefined 
          
         
+-- ------------------------------------------------------------
+-- Num 
+-- ------------------------------------------------------------
+
+liftSE :: Syntax Expr -> Exp a
+liftSE = Exp . Expr 
+
+instance Num a => Num (Exp a) where
+  (+) a b = liftSE $ Op Add [toExp a, toExp b]
+  (-) a b = liftSE $ Op Sub [toExp a, toExp b]
+  (*) a b = liftSE $ Op Mul [toExp a, toExp b]
+
+  abs = undefined
+  signum = undefined
+
+  fromInteger i = liftSE $ Constant (VInt (fromInteger i))  
+
+
+
+-- Integer division
+div :: Integral a => Exp a -> Exp a -> Exp a
+div a b = liftSE $ Op Div [toExp a, toExp b] 
