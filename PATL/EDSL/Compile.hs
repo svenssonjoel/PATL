2c6f97bf73f416cce941cf42782e0e843fae274e
Joel Svensson <svenssonjoel@yahoo.se>
2016-07-13 22:09:52 +0200

    sketching up a graphToAst function, tricky

--- PATL/EDSL/Compile.hs
+++ PATL/EDSL/Compile.hs
@@ -13,7 +13,9 @@ import Data.Reify
 import Data.Reify.Graph
 
 import qualified Data.Map as M
-import qualified Data.Set as Set
+-- import qualified Data.Set as Set
+
+import Control.Monad.State
 
 
 -- This is the end goal
@@ -39,9 +41,59 @@ genGraph = reifyGraph
 --  
 -- ------------------------------------------------------------
 
+type G a = State (M.Map Unique A.Exp) a 
 
 graphToAST :: Graph Syntax -> Maybe A.Exp
-graphToAST (Graph edges root) = undefined 
+graphToAST (Graph edges root) = evalState ( doIt root ) M.empty 
+  where
+    doIt ::  Unique -> G (Maybe A.Exp)
+    doIt nid =
+      case lookup nid edges of -- currently list lookup 
+        Nothing -> return Nothing
+        Just node ->
+          case node of
+            -- A constant, just convert it to AST  
+            (Constant v) ->
+                 let ast_node = A.Constant v
+                 in return $ Just ast_node
+            -- These variables can only come from Lambdas in the EDSL!
+            (Var (FunArg i)) -> return $ Just (A.Var ("l" ++ show i))
+            (TuneParam tp)   ->
+              do bound <- get
+                 -- look it up and return if found. otherwise return self
+                 case M.lookup nid bound of
+                   Nothing -> return $ Just (A.TuneParam tp)
+                   Just v  -> return $ Just v
+
+            -- Introduce let bindings for "ss" (and treat recursively) 
+            (Tuple ss) ->
+              do bound <- get
+                 
+                 let vars = [A.Var ("v" ++ show i) | i <- ss]
+                     done = map (\nodid -> M.lookup nodid bound) ss :: [Maybe A.Exp]
+                     dv   = zip done ss -- indicates which should be introduced here
+                 fmap Just $ bindMissing dv (A.Tuple) 
+                 
+                 
+    bindMissing :: [(Maybe A.Exp, Unique)] -> ([A.Exp] -> A.Exp) -> G A.Exp 
+    bindMissing xs f = do (acc,f') <- doLet xs [] f
+                          return $ f' (reverse acc)
+      where
+        doLet ((m,u):xs) acc f
+          | m == Nothing =
+            do let v = (A.Var ("v" ++ show u))
+               e <- doIt u
+               case e of
+                 Nothing -> error "CLEANUP" 
+                 Just e' -> do
+                   bound <- get
+                   put (M.insert u v bound)
+                   return (v:acc, \vars -> A.Let ("v" ++ show u) e' (f vars))
+                                  
+                
+      
+  
+
 
     
 
